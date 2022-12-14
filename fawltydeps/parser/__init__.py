cc854fab9aa5052a8f8d0e6823f3c8b4ca0e34d7
Johan Herland <johan.herland@tweag.io>
2022-12-14 21:45:31 +0100

    parser.parse_imports(): Handle 'from ... import ...' statements
    Also verify that we already correctly handle 'import ... as ...'.
    
    Improved-by: Nour El Mawass <nour.elmawass@tweag.io>

--- fawltydeps/parser/__init__.py
+++ fawltydeps/parser/__init__.py
@@ -10,6 +10,9 @@ logger = logging.getLogger(__name__)
 def parse_imports(code: str) -> Iterator[str]:
     for node in ast.walk(ast.parse(code)):
         if isinstance(node, ast.Import):
-            logger.debug(ast.dump(node, indent=4))
+            logger.debug(ast.dump(node))
             for alias in node.names:
                 yield alias.name
+        elif isinstance(node, ast.ImportFrom):
+            logger.debug(ast.dump(node))
+            yield node.module
