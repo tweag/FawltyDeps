31b5737458bd6ae95c86b43a124979c7f5b9c3d2
Johan Herland <johan.herland@tweag.io>
2022-12-14 21:45:36 +0100

    extract_imports: Make sure that filename information is not lost
    We don't yet know how to report errors to the end user, but one thing we
    know is that telling the user there is a syntax error in their code, but
    not telling them _where_ is not very good design...
    
    Make sure that we pass filenames into the ast.parse() call, so that any
    resulting errors generated by the ast module contain source information.

--- tests/test_extract_imports_errors.py
+++ tests/test_extract_imports_errors.py
@@ -13,3 +13,28 @@ def test_parse_code_failures_propagates_SyntaxError():
         """)
     with pytest.raises(SyntaxError):
         list(parse_code(code))
+
+
+def test_parse_file_failures_contain_filename(tmp_path):
+    code = dedent("""\
+        This is not Python code
+        """)
+    script = tmp_path / "test.py"
+    script.write_text(code)
+
+    with pytest.raises(SyntaxError) as exc_info:
+        list(parse_file(script))
+    assert exc_info.value.filename == str(script)
+
+
+def test_parse_dir_with_syntax_error_contains_filename(tmp_path):
+    code = dedent("""\
+        This file is littered with Python syntax errors...
+        import word.
+        """)
+    script = tmp_path / "python_with_syntax_error.py"
+    script.write_text(code)
+
+    with pytest.raises(SyntaxError) as exc_info:
+        list(parse_dir(tmp_path))
+    assert exc_info.value.filename == str(script)
