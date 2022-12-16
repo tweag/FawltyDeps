fc23f995786ccab6816b7a14960afffe8659d6c1
Nour El Mawass <nour.elmawass@tweag.io>
2022-12-16 20:51:11 +0100

    Parametrize tests

--- tests/test_compare_imports_to_dependencies.py
+++ tests/test_compare_imports_to_dependencies.py
@@ -1,67 +1,60 @@
 """Test the imports to dependencies comparison function."""
+import pytest
+from fawltydeps.check import compare_imports_to_dependencies, DependencyComparison
 
-from fawltydeps.check import compare_imports_to_dependencies
-
-
-def test_no_import_no_dependencies():
-    imports = []
-    dependencies = []
-    expected = (set(), set())
-    obtained = compare_imports_to_dependencies(imports, dependencies)
-    assert obtained == expected
+testdata = []
 
 
-def test_stdlib_import_no_dependencies():
-    imports = ["sys"]
-    dependencies = []
-    expected = (set(), set())
-    obtained = compare_imports_to_dependencies(imports, dependencies)
-    assert obtained == expected
-
-
-def test_non_stdlib_import_no_dependencies():
-    imports = ["pandas"]
-    dependencies = []
-    expected = (set(["pandas"]), set())
-    obtained = compare_imports_to_dependencies(imports, dependencies)
-    assert obtained == expected
-
-
-def test_no_imports_one_dependency():
-    imports = []
-    dependencies = ["pandas"]
-    expected = (set(), set(["pandas"]))
-    obtained = compare_imports_to_dependencies(imports, dependencies)
-    assert obtained == expected
-
-
-def test_mixed_imports_non_stdlib_dependency():
-    imports = ["sys", "pandas"]
-    dependencies = ["pandas"]
-    expected = (set(), set())
-    obtained = compare_imports_to_dependencies(imports, dependencies)
-    assert obtained == expected
-
-
-def test_mixed_imports_no_dependencies():
-    imports = ["sys", "pandas"]
-    dependencies = []
-    expected = (set(["pandas"]), set())
-    obtained = compare_imports_to_dependencies(imports, dependencies)
-    assert obtained == expected
-
-
-def test_stdlib_import_and_non_stdlib_dependency():
-    imports = ["sys"]
-    dependencies = ["pandas"]
-    expected = (set(), set(["pandas"]))
-    obtained = compare_imports_to_dependencies(imports, dependencies)
-    assert obtained == expected
-
-
-def test_mixed_imports_with_unused_and_undeclared_dependencies():
-    imports = ["sys", "pandas", "numpy"]
-    dependencies = ["pandas", "scipy"]
-    expected = (set(["numpy"]), set(["scipy"]))
+@pytest.mark.parametrize(
+    "imports,dependencies,expected",
+    [
+        pytest.param(
+            [], [], DependencyComparison(set(), set()), id="no_import_no_dependencies"
+        ),
+        pytest.param(
+            ["sys"],
+            [],
+            DependencyComparison(set(), set()),
+            id="stdlib_import_no_dependencies",
+        ),
+        pytest.param(
+            ["pandas"],
+            [],
+            DependencyComparison(set(["pandas"]), set()),
+            id="non_stdlib_import_no_dependencies",
+        ),
+        pytest.param(
+            [],
+            ["pandas"],
+            DependencyComparison(set(), set(["pandas"])),
+            id="no_imports_one_dependency",
+        ),
+        pytest.param(
+            ["sys", "pandas"],
+            ["pandas"],
+            DependencyComparison(set(), set()),
+            id="mixed_imports_non_stdlib_dependency",
+        ),
+        pytest.param(
+            ["sys", "pandas"],
+            [],
+            DependencyComparison(set(["pandas"]), set()),
+            id="mixed_imports_no_dependencies",
+        ),
+        pytest.param(
+            ["sys"],
+            ["pandas"],
+            DependencyComparison(set(), set(["pandas"])),
+            id="stdlib_import_and_non_stdlib_dependency",
+        ),
+        pytest.param(
+            ["sys", "pandas", "numpy"],
+            ["pandas", "scipy"],
+            DependencyComparison(set(["numpy"]), set(["scipy"])),
+            id="mixed_imports_with_unused_and_undeclared_dependencies",
+        ),
+    ],
+)
+def test_compare_imports_to_dependencies(imports, dependencies, expected):
     obtained = compare_imports_to_dependencies(imports, dependencies)
     assert obtained == expected
