109c15d684a868a600a65b5d4d51d60f1a669c5b
Johan Herland <johan.herland@tweag.io>
2023-01-05 00:18:59 +0100

    main: Teach command-line interface to list dependencies
    This adds the --deps option (similar to --code) to tell fawltydeps where
    to look for declared dependencies.
    
    It also introduces two new options to choose which action fawltydeps
    should perform:
    
     --list-imports: List the extracted imports and exit
     --list-deps: List the extracted dependencies and exit
    
    When none of these is given, we currently default to --list-imports.
    
    The available actions will change soon, as will the default action.

--- fawltydeps/main.py
+++ fawltydeps/main.py
@@ -2,16 +2,60 @@
 
 import argparse
 import logging
+from enum import Enum, auto
+from operator import itemgetter
 from pathlib import Path
+from typing import Set
 
 from fawltydeps import extract_imports
+from fawltydeps.extract_dependencies import extract_dependencies
 
 logger = logging.getLogger(__name__)
+
+
+class Action(Enum):
+    """Actions available to the command-line interface."""
+
+    LIST_IMPORTS = auto()
+    LIST_DEPS = auto()
+
+
+def perform_actions(actions: Set[Action], code: Path, deps: Path) -> int:
+    if actions & {Action.LIST_IMPORTS}:
+        extracted_imports = set(extract_imports.parse_any_arg(code))
+        for name in sorted(extracted_imports):
+            # TODO: Add location information to extracted imports
+            print(name)
+
+    if actions & {Action.LIST_DEPS}:
+        extracted_deps = set(extract_dependencies(deps))
+        # Sort dependencies by location, then by name
+        for name, location in sorted(extracted_deps, key=itemgetter(1, 0)):
+            print(f"{name}: {location}")
+
+    return 0
 
 
 def main() -> int:
     """Command-line entry point."""
     parser = argparse.ArgumentParser(description=__doc__)
+
+    select_action = parser.add_mutually_exclusive_group()
+    select_action.add_argument(
+        "--list-imports",
+        dest="actions",
+        action="store_const",
+        const={Action.LIST_IMPORTS},
+        help="List imports extracted from code and exit",
+    )
+    select_action.add_argument(
+        "--list-deps",
+        dest="actions",
+        action="store_const",
+        const={Action.LIST_DEPS},
+        help="List declared dependencies and exit",
+    )
+
     parser.add_argument(
         "--code",
         type=Path,
@@ -19,6 +63,16 @@ def main() -> int:
         help=(
             "Code to parse for import statements (file or directory, use '-' "
             "to read code from stdin; defaults to the current directory)"
+        ),
+    )
+    parser.add_argument(
+        "--deps",
+        type=Path,
+        default=Path.cwd(),
+        help=(
+            "Where to find dependency declarations (file or directory, defaults"
+            " to looking for requirements.txt/.in/setup.py/pyproject.toml in "
+            "the current directory)"
         ),
     )
     parser.add_argument(
@@ -42,15 +96,10 @@ def main() -> int:
         level=logging.WARNING + 10 * (args.quiet - args.verbose),
     )
 
+    if args.actions is None:  # Provide default action
+        args.actions = {Action.LIST_IMPORTS}
+
     try:
-        extracted_imports = set(extract_imports.parse_any_arg(args.code))
+        return perform_actions(args.actions, args.code, args.deps)
     except extract_imports.ParseError as e:
-        parser.error(e.msg)
-
-    # TODO: Extract declared dependencies
-    # TODO: Pass imports and dependencies to comparator.
-    # Until then:
-    for imp in sorted(extracted_imports):
-        print(imp)
-
-    return 0
+        return parser.error(e.msg)
