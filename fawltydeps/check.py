18cdc25a4b37d3dbf14f0f65218a911d579c4c68
Maria Knorps <maria.knorps@tweag.io>
2023-01-17 19:31:23 +0100

    Use ParsedImport and DeclaredDependency
    type for comparison
    perform_actions use now lists not sets

--- fawltydeps/check.py
+++ fawltydeps/check.py
@@ -1,8 +1,11 @@
 "Compare imports and dependencies"
 
-from typing import Iterable, NamedTuple, Set
+from typing import List, NamedTuple, Set
 
 import isort
+
+from fawltydeps.extract_dependencies import DeclaredDependency
+from fawltydeps.extract_imports import ParsedImport
 
 DependencyComparison = NamedTuple(
     "DependencyComparison", [("undeclared", Set[str]), ("unused", Set[str])]
@@ -10,17 +13,19 @@ DependencyComparison = NamedTuple(
 
 
 def compare_imports_to_dependencies(
-    imports: Iterable[str], dependencies: Iterable[str]
+    imports: List[ParsedImport], dependencies: List[DeclaredDependency]
 ) -> DependencyComparison:
     """
     Compares imports to dependencies
 
     Returns set of undeclared non stdlib imports and set of unused dependencies
     """
+    imports_names = [i.name for i in imports]
+    dependencies_names = [d.name for d in dependencies]
     non_stdlib_imports = {
-        module for module in imports if isort.place_module(module) != "STDLIB"
+        module for module in imports_names if isort.place_module(module) != "STDLIB"
     }
-    unique_dependencies = set(dependencies)
+    unique_dependencies = set(dependencies_names)
     undeclared = non_stdlib_imports - unique_dependencies
     unused = unique_dependencies - non_stdlib_imports
     return DependencyComparison(undeclared, unused)
