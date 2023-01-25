3472ebf7eec644871d20e5033371e857b74c3554
Maria Knorps <maria.knorps@tweag.io>
2023-01-25 10:36:49 +0100

    Add undeclared and unused project examples

--- tests/test_sample_projects.py
+++ tests/test_sample_projects.py
@@ -14,14 +14,12 @@ Sample projects from `tests/project_gallery` are used via fixtures.
 import os
 import shutil
 import sys
-from itertools import groupby
 from pathlib import Path
 
 import pytest
 
-from fawltydeps.check import compare_imports_to_dependencies
-from fawltydeps.extract_dependencies import extract_dependencies
-from fawltydeps.extract_imports import parse_any_arg
+from fawltydeps.main import Action, Analysis
+from fawltydeps.types import FileLocation
 
 if sys.version_info >= (3, 11):
     import tomllib  # pylint: disable=E1101
@@ -35,7 +33,7 @@ pytestmark = pytest.mark.integration
 @pytest.fixture
 def datadir(tmp_path: Path) -> Path:
 
-    test_dir = "./tests/projects_gallery"
+    test_dir = "./tests/sample_projects"
     if os.path.isdir(test_dir):
         shutil.copytree(test_dir, tmp_path / "data")
 
@@ -44,18 +42,23 @@ def datadir(tmp_path: Path) -> Path:
 
 def test_integration_compare_imports_to_dependencies(datadir):
 
-    project_path = datadir / "file__requirements"
-    dependencies = list(extract_dependencies(project_path))
-    imports = parse_any_arg(project_path)
+    # project_path = datadir / "file__requirements_unused"
+    project_path = datadir / "file__requirements_undeclared"
 
-    result = compare_imports_to_dependencies(imports=imports, dependencies=dependencies)
-    unused_dependencies_locations = {
-        name: [l.location.name for l in locations]
-        for name, locations in groupby(dependencies, key=lambda x: x.name)
-        if name in result.unused
-    }
+    actions = {Action.REPORT_UNDECLARED, Action.REPORT_UNUSED}
+    analysis = Analysis.create(actions, code=project_path, deps=project_path)
+
     with (project_path / "expected.toml").open("rb") as f:
         expected = tomllib.load(f)
 
-    assert unused_dependencies_locations == expected["unused_deps"]
-    assert result.undeclared == expected["undeclared_deps"]
+    # parse FileLocation properly
+    expected["undeclared_deps"] = {
+        k: [
+            FileLocation(project_path / value["path"], value.get("lineno"))
+            for value in v
+        ]
+        for k, v in expected["undeclared_deps"].items()
+    }
+
+    assert analysis.unused_deps == set(expected["unused_deps"].keys())
+    assert analysis.undeclared_deps == expected["undeclared_deps"]
