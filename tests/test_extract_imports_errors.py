22fa13834876a8ac32ce67667b2e15ec76e6f50a
Nour El Mawass <nour.elmawass@tweag.io>
2023-02-02 16:33:54 +0100

    Handle `SyntaxError` on parsing imports from Python code in `parse_code`
    This replaces raising `SyntaxError` on invalid Python code in
    `parse_code`with an error log containing the location. The error will
    subsequently be suppressed from other functions calling `parse_code`,
    namely `parse_python_file`, `parse_notebook_file` and `parse_dir`.
    Furthermore, the execution will not halt and parsing imports will be
    resumed as follows:
    - Other files in the directory will be parsed for imports in `parse_dir`
    - Other cells in the notebook on `parse_notebook_file`
    - An empty list of dependencies will be reported on `parse_code` or
    `parse_python_file`.
    
    In all cases, other actions requested by the CLI args will be continued.
    
    This commit does not take care of suppressing other errors on parsing
    imports from notebook files.

--- tests/test_extract_imports_errors.py
+++ tests/test_extract_imports_errors.py
@@ -11,38 +11,7 @@ from fawltydeps.extract_imports import (
     parse_notebook_file,
     parse_python_file,
 )
-from fawltydeps.types import Location
-
-
-def test_parse_code__on_parse_error__propagates_SyntaxError():
-    code = "This is not Python code\n"
-    with pytest.raises(SyntaxError):
-        list(parse_code(code, source=Location("<stdin>")))
-
-
-def test_parse_python_file__on_parse_error__SyntaxError_contains_filename(tmp_path):
-    code = "This is not Python code\n"
-    script = tmp_path / "test.py"
-    script.write_text(code)
-
-    with pytest.raises(SyntaxError) as exc_info:
-        list(parse_python_file(script))
-    assert exc_info.value.filename == str(script)
-
-
-def test_parse_dir__on_parse_error__SyntaxError_contains_filename(tmp_path):
-    code = dedent(
-        """\
-        This file is littered with Python syntax errors...
-        import word.
-        """
-    )
-    script = tmp_path / "python_with_syntax_error.py"
-    script.write_text(code)
-
-    with pytest.raises(SyntaxError) as exc_info:
-        list(parse_dir(tmp_path))
-    assert exc_info.value.filename == str(script)
+from fawltydeps.types import Location, ParsedImport
 
 
 def test_parse_notebook_file__on_parse_error__propagates_SyntaxError(tmp_path):
@@ -92,34 +61,6 @@ def test_parse_notebook_file__on_parse_error__SyntaxError_raised_with_msg(tmp_pa
     assert exc_info.value.msg == f"Cannot parse code from {script}[1]."
 
 
-def test_parse_notebook_file__on_invalid_python__SyntaxError_raised_with_msg(tmp_path):
-    code = dedent(
-        """\
-        {
-            "metadata": {
-                "language_info": {
-                    "name": "Python"
-                }
-            },
-            "cells": [
-            {
-                "cell_type": "code",
-                "source": [
-                    "import json..."
-                ]
-            }
-        ]
-        }
-       """
-    )
-    script = tmp_path / "test.ipynb"
-    script.write_text(code)
-
-    with pytest.raises(SyntaxError) as exc_info:
-        list(parse_notebook_file(script))
-    assert exc_info.value.msg == f"Cannot parse code from {script}[1]."
-
-
 def test_parse_notebook_file__on_invalid_json__JSONDecodeError_raised_with_msg(
     tmp_path,
 ):
@@ -137,3 +78,78 @@ def test_parse_notebook_file__on_invalid_json__JSONDecodeError_raised_with_msg(
 
     with pytest.raises(json.decoder.JSONDecodeError):
         list(parse_notebook_file(script))
+
+
+def test_parse_code__on_parse_error__logs_error(caplog):
+    code = dedent(
+        """
+        import pandas
+        This is not Python code
+        """
+    )
+    source = Location("<stdin>")
+    expect = []
+    assert list(parse_code(code, source=source)) == expect
+    assert f"Could not parse code from {source}" in caplog.text
+
+
+def test_parse_file__on_syntax_error__logs_error(tmp_path, caplog):
+    code = "This is not Python code\n"
+    script = tmp_path / "test.py"
+    script.write_text(code)
+
+    expect = []
+    assert list(parse_python_file(script)) == expect
+    assert f"Could not parse code from {script}" in caplog.text
+
+
+def test_parse_dir__on_parse_error__error_log_contains_filename(tmp_path, caplog):
+    code = dedent(
+        """\
+        This file is littered with Python syntax errors...
+        import word.
+        """
+    )
+    script = tmp_path / "python_with_syntax_error.py"
+    script.write_text(code)
+
+    expect = []
+    assert list(parse_dir(tmp_path)) == expect
+    assert f"Could not parse code from {script}" in caplog.text
+
+
+def test_parse_notebook_file__on_invalid_python_one_cell__logs_error_and_continues(
+    tmp_path, caplog
+):
+    code = dedent(
+        """\
+        {
+            "metadata": {
+                "language_info": {
+                    "name": "Python"
+                }
+            },
+            "cells": [
+            {
+                "cell_type": "code",
+                "source": [
+                    "import json..."
+                ]
+            },
+            {
+                "cell_type": "code",
+                "source": [
+                    "import pandas"
+                ]
+            }
+        ]
+        }
+       """
+    )
+    script = tmp_path / "test.ipynb"
+    script.write_text(code)
+
+    assert list(parse_notebook_file(script)) == [
+        ParsedImport("pandas", Location(script, lineno=1, cellno=2))
+    ]
+    assert f"Could not parse code from {script}[1]" in caplog.text
