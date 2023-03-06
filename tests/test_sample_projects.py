6514c8c2b4cbd3ba5bf785479efdaee84744b696
Johan Herland <johan.herland@tweag.io>
2023-03-06 14:03:42 +0100

    test_sample_projects: Add optional checks and more verbose debug info
    Add some print() statements while testing each sample project:
     - Print the name and description of the project
     - Print a line before each check that we perform
    
    These prints are only visible when the test fails, and helps us more
    easily see which project and which check is failing.
    
    Also add optional verification of analysis.imports and .declared_deps.
    If the "imports = [ ... ]" or "declared_deps = [ ... ]" exist in the
    expected.toml, then we will verify them in addition to the verification
    of .undeclared_deps and .unused_deps. Otherwise, we only check the
    latter two (which are mandatory).

--- tests/test_sample_projects.py
+++ tests/test_sample_projects.py
@@ -55,14 +55,30 @@ def test_integration_analysis_on_sample_projects__(project_path):
     analysis = Analysis.create(settings)
 
     with (project_path / "expected.toml").open("rb") as f:
-        expected = tomllib.load(f)
+        experiment = tomllib.load(f)
 
-    actual_unused = {u.name for u in analysis.unused_deps}
-    expect_unused = set(expected.get("analysis_result", {}).get("unused_deps", []))
-    assert actual_unused == expect_unused
+    print(experiment["project"].get("name"))
+    print(experiment["project"].get("description"))
+    expected = experiment.get("analysis_result", {})
 
+    if "imports" in expected:
+        print("Verifying 'imports'...")
+        actual_imports = {i.name for i in analysis.imports}
+        expect_imports = set(expected["imports"])
+        assert actual_imports == expect_imports
+
+    if "declared_deps" in expected:
+        print("Verifying 'declared_deps'...")
+        actual_declared = {d.name for d in analysis.declared_deps}
+        expect_declared = set(expected["declared_deps"])
+        assert actual_declared == expect_declared
+
+    print("Verifying 'undeclared_deps'...")
     actual_undeclared = {u.name for u in analysis.undeclared_deps}
-    expect_undeclared = set(
-        expected.get("analysis_result", {}).get("undeclared_deps", [])
-    )
+    expect_undeclared = set(expected.get("undeclared_deps", []))
     assert actual_undeclared == expect_undeclared
+
+    print("Verifying 'unused_deps'...")
+    actual_unused = {u.name for u in analysis.unused_deps}
+    expect_unused = set(expected.get("unused_deps", []))
+    assert actual_unused == expect_unused
