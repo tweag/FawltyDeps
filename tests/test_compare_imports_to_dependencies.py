3ce1bdecf334612373d4854420b11c87b62c110f
Johan Herland <johan.herland@tweag.io>
2023-03-09 16:19:45 +0100

    test_compare_imports_to_dependencies: Log inputs to tests
    This makes failing tests document their inputs, which is helpful when
    debugging.

--- tests/test_compare_imports_to_dependencies.py
+++ tests/test_compare_imports_to_dependencies.py
@@ -1,4 +1,6 @@
 """Test the imports to dependencies comparison function."""
+import logging
+
 import pytest
 
 from fawltydeps.check import calculate_undeclared, calculate_unused
@@ -6,10 +8,15 @@ from fawltydeps.settings import Settings
 
 from .utils import test_vectors
 
+logger = logging.getLogger(__name__)
+
 
 @pytest.mark.parametrize("vector", [pytest.param(v, id=v.id) for v in test_vectors])
 def test_calculate_undeclared(vector):
     settings = Settings(ignore_undeclared=vector.ignore_undeclared)
+    logger.info(f"imports: {vector.imports!r}")
+    logger.info(f"declared_deps: {vector.declared_deps!r}")
+    logger.info(f"resolved_deps: {vector.expect_resolved_deps!r}")
     actual = calculate_undeclared(vector.imports, vector.expect_resolved_deps, settings)
     assert actual == vector.expect_undeclared_deps
 
@@ -17,6 +24,9 @@ def test_calculate_undeclared(vector):
 @pytest.mark.parametrize("vector", [pytest.param(v, id=v.id) for v in test_vectors])
 def test_calculate_unused(vector):
     settings = Settings(ignore_unused=vector.ignore_unused)
+    logger.info(f"imports: {vector.imports!r}")
+    logger.info(f"declared_deps: {vector.declared_deps!r}")
+    logger.info(f"resolved_deps: {vector.expect_resolved_deps!r}")
     actual = calculate_unused(
         vector.imports, vector.declared_deps, vector.expect_resolved_deps, settings
     )
