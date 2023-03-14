5120fd0e947f463c3a69b28c5c07aa1b70f019c9
Johan Herland <johan.herland@tweag.io>
2023-03-14 21:11:15 +0100

    test_extract_declared_dependencies_pyproject_toml: Remove unnecessary dedent()

--- tests/test_extract_declared_dependencies_pyproject_toml.py
+++ tests/test_extract_declared_dependencies_pyproject_toml.py
@@ -1,6 +1,5 @@
 """Test extracting dependencies from pyproject.toml"""
 import logging
-from textwrap import dedent
 
 import pytest
 
@@ -12,70 +11,62 @@ from fawltydeps.types import DeclaredDependency, Location
     "pyproject_toml,expected_deps",
     [
         pytest.param(
-            dedent(
-                """\
-                [tool.poetry]
+            """\
+            [tool.poetry]
 
-                [tool.poetry.dependencies]
-                python = "^3.8"
-                isort = "^5.10"
-                tomli = "^2.0.1"
-                """
-            ),
+            [tool.poetry.dependencies]
+            python = "^3.8"
+            isort = "^5.10"
+            tomli = "^2.0.1"
+            """,
             ["isort", "tomli"],
             id="poetry_main_dependencies",
         ),
         pytest.param(
-            dedent(
-                """\
-                [tool.poetry]
+            """\
+            [tool.poetry]
 
-                [tool.poetry.group.dev.dependencies]
-                black = "^22"
-                mypy = "^0.991"
+            [tool.poetry.group.dev.dependencies]
+            black = "^22"
+            mypy = "^0.991"
 
-                [tool.poetry.group.test.dependencies]
-                pytest = "^5.0.0"
-                """
-            ),
+            [tool.poetry.group.test.dependencies]
+            pytest = "^5.0.0"
+            """,
             ["black", "mypy", "pytest"],
             id="poetry_group_dependencies",
         ),
         pytest.param(
-            dedent(
-                """\
-                [tool.poetry]
+            """\
+            [tool.poetry]
 
-                [tool.poetry.extras]
-                test = ["pytest < 5.0.0", "pytest-cov[all]"]
-                dev = ["pylint >= 2.15.8"]
-                """
-            ),
+            [tool.poetry.extras]
+            test = ["pytest < 5.0.0", "pytest-cov[all]"]
+            dev = ["pylint >= 2.15.8"]
+            """,
             ["pytest", "pytest-cov", "pylint"],
             id="poetry_extra_dependencies",
         ),
         pytest.param(
-            dedent(
-                """\
-                [tool.poetry]
+            """\
+            [tool.poetry]
 
-                [tool.poetry.dependencies]
-                python = "^3.8"
-                isort = "^5.10"
-                tomli = "^2.0.1"
+            [tool.poetry.dependencies]
+            python = "^3.8"
+            isort = "^5.10"
+            tomli = "^2.0.1"
 
-                [tool.poetry.group.dev.dependencies]
-                black = "^22"
-                mypy = "^0.991"
+            [tool.poetry.group.dev.dependencies]
+            black = "^22"
+            mypy = "^0.991"
 
-                [tool.poetry.group.experimental.dependencies]
-                django = "^2.1"
+            [tool.poetry.group.experimental.dependencies]
+            django = "^2.1"
 
-                [tool.poetry.extras]
-                test = ["pytest < 5.0.0", "pytest-cov[all]"]
-                dev = ["pylint >= 2.15.8"]
-                """
-            ),
+            [tool.poetry.extras]
+            test = ["pytest < 5.0.0", "pytest-cov[all]"]
+            dev = ["pylint >= 2.15.8"]
+            """,
             [
                 "isort",
                 "tomli",
@@ -89,75 +80,67 @@ from fawltydeps.types import DeclaredDependency, Location
             id="poetry_main_group_and_extra_dependencies",
         ),
         pytest.param(
-            dedent(
-                """\
-                [project]
-                name = "fawltydeps"
+            """\
+            [project]
+            name = "fawltydeps"
 
-                dependencies = ["isort", "django>2.1; os_name != 'nt'"]
-                """
-            ),
+            dependencies = ["isort", "django>2.1; os_name != 'nt'"]
+            """,
             ["isort", "django"],
             id="pep621_main_dependencies",
         ),
         pytest.param(
-            dedent(
-                """\
-                [project]
+            """\
+            [project]
 
-                [project.optional-dependencies]
-                test = ["pytest < 5.0.0", "pytest-cov[all]"]
-                dev = ["pylint >= 2.15.8"]
-                """
-            ),
+            [project.optional-dependencies]
+            test = ["pytest < 5.0.0", "pytest-cov[all]"]
+            dev = ["pylint >= 2.15.8"]
+            """,
             ["pytest", "pytest-cov", "pylint"],
             id="pep621_optional_dependencies",
         ),
         pytest.param(
-            dedent(
-                """\
-                [project]
-                name = "fawltydeps"
+            """\
+            [project]
+            name = "fawltydeps"
 
-                dependencies = ["isort", "django>2.1; os_name != 'nt'"]
+            dependencies = ["isort", "django>2.1; os_name != 'nt'"]
 
-                [project.optional-dependencies]
-                test = ["pytest < 5.0.0", "pytest-cov[all]"]
-                dev = ["pylint >= 2.15.8"]
-                """
-            ),
+            [project.optional-dependencies]
+            test = ["pytest < 5.0.0", "pytest-cov[all]"]
+            dev = ["pylint >= 2.15.8"]
+            """,
             ["isort", "django", "pytest", "pytest-cov", "pylint"],
             id="pep_621_main_and_optional_dependencies",
         ),
         pytest.param(
-            dedent(
-                """\
-                [project]
-                name = "fawltydeps"
+            """\
+            [project]
+            name = "fawltydeps"
 
-                dependencies = ["pandas", "pydantic>1.10.4"]
+            dependencies = ["pandas", "pydantic>1.10.4"]
 
-                [project.optional-dependencies]
-                dev = ["pylint >= 2.15.8"]
+            [project.optional-dependencies]
+            dev = ["pylint >= 2.15.8"]
 
-                [tool.poetry]
-                [tool.poetry.dependencies]
-                python = "^3.8"
-                isort = "^5.10"
-                tomli = "^2.0.1"
+            [tool.poetry]
+            [tool.poetry.dependencies]
+            python = "^3.8"
+            isort = "^5.10"
+            tomli = "^2.0.1"
 
-                [tool.poetry.group.dev.dependencies]
-                black = "^22"
-                mypy = "^0.991"
+            [tool.poetry.group.dev.dependencies]
+            black = "^22"
+            mypy = "^0.991"
 
-                [tool.poetry.group.experimental.dependencies]
-                django = "^2.1"
+            [tool.poetry.group.experimental.dependencies]
+            django = "^2.1"
 
-                [tool.poetry.extras]
-                alpha = ["pytorch < 1.12.1", "numpy >= 1.17.2"]
-                dev = ["flake >= 5.0.1"]
-                """
-            ),
+            [tool.poetry.extras]
+            alpha = ["pytorch < 1.12.1", "numpy >= 1.17.2"]
+            dev = ["flake >= 5.0.1"]
+            """,
             [
                 "pandas",
                 "pydantic",
@@ -190,105 +173,89 @@ def test_parse_pyproject_content__pep621_or_poetry_dependencies__yields_dependen
     "pyproject_toml,expected,metadata_standard,field_types",
     [
         pytest.param(
-            dedent(
-                """\
-                [tool.poetry]
-                dependencies = ["pylint"]
-                """
-            ),
+            """\
+            [tool.poetry]
+            dependencies = ["pylint"]
+            """,
             [],
             "Poetry",
             ["main"],
             id="poetry_dependencies_as_one_element_list",
         ),
         pytest.param(
-            dedent(
-                """\
-                [tool.poetry]
-                dependencies = "pylint"
-                """
-            ),
+            """\
+            [tool.poetry]
+            dependencies = "pylint"
+            """,
             [],
             "Poetry",
             ["main"],
             id="poetry_dependencies_as_str",
         ),
         pytest.param(
-            dedent(
-                """\
-                [tool.poetry]
-                [tool.poetry.group.dev]
-                dependencies = ["black > 22", "mypy"]
-                """
-            ),
+            """\
+            [tool.poetry]
+            [tool.poetry.group.dev]
+            dependencies = ["black > 22", "mypy"]
+            """,
             [],
             "Poetry",
             ["group"],
             id="poetry_dependencies_as_list",
         ),
         pytest.param(
-            dedent(
-                """\
-                [tool.poetry]
-                [tool.poetry.extras]
-                test = "pytest"
-                """
-            ),
+            """\
+            [tool.poetry]
+            [tool.poetry.extras]
+            test = "pytest"
+            """,
             [],
             "Poetry",
             ["extra"],
             id="poetry_extra_requirements_as_str_instead_of_list",
         ),
         pytest.param(
-            dedent(
-                """\
-                [tool.poetry]
-                extras = ["pytest"]
-                """
-            ),
+            """\
+            [tool.poetry]
+            extras = ["pytest"]
+            """,
             [],
             "Poetry",
             ["extra"],
             id="poetry_extra_requirements_as_list_instead_of_dict",
         ),
         pytest.param(
-            dedent(
-                """\
-                [tool.poetry]
+            """\
+            [tool.poetry]
 
-                dependencies = ["pylint"]
+            dependencies = ["pylint"]
 
-                [tool.poetry.group.dev]
-                dependencies = ["black > 22", "mypy"]
+            [tool.poetry.group.dev]
+            dependencies = ["black > 22", "mypy"]
 
-                [tool.poetry.extras]
-                black = "^22"
-                """
-            ),
+            [tool.poetry.extras]
+            black = "^22"
+            """,
             [],
             "Poetry",
             ["main", "group", "extra"],
             id="poetry_all_dependencies_malformatted",
         ),
         pytest.param(
-            dedent(
-                """\
-                [project.dependencies]
-                pylint = ""
-                """
-            ),
+            """\
+            [project.dependencies]
+            pylint = ""
+            """,
             [],
             "PEP621",
             ["main"],
             id="pep621_dependencies_as_dict_instead_of_list",
         ),
         pytest.param(
-            dedent(
-                """\
-                [project]
-                optional-dependencies = ["pylint"]
-                """
-            ),
+            """\
+            [project]
+            optional-dependencies = ["pylint"]
+            """,
             [],
             "PEP621",
             ["optional"],
@@ -316,23 +283,19 @@ def test_parse_pyproject_content__malformatted_poetry_dependencies__yields_no_de
     "pyproject_toml,expected,expected_logs",
     [
         pytest.param(
-            dedent(
-                """\
-                [project]
-                name = "fawltydeps"
-                """
-            ),
+            """\
+            [project]
+            name = "fawltydeps"
+            """,
             [],
             [("PEP621", "main"), ("PEP621", "optional")],
             id="missing_pep621_fields",
         ),
         pytest.param(
-            dedent(
-                """\
-                [tool.poetry]
-                name = "fawltydeps"
-                """
-            ),
+            """\
+            [tool.poetry]
+            name = "fawltydeps"
+            """,
             [],
             [
                 ("PEP621", "main"),
