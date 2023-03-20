c0689271a3379dc247325a5ff7cf4a5b64ea43d9
Nour El Mawass <nour.elmawass@tweag.io>
2023-03-20 19:02:15 +0100

    Base mappings on a local env set up with declared deps
    - Not user facing yet
    - No cache mechanism yet
    - Will install latest available version of a package
    - Will not solve for a set of packages versions that respect constraints
    - No possibility to only install deps not available in the Python env
    - Uses the Python version currently ruuning FawltyDeps. Might want to
      consider inferring that from the project configuration.
    
    Use a context manager to obtain packages from a temp venv

---
+++ tests/test_pull_deps.py
@@ -0,0 +1,43 @@
+"""Test the behavior of Analysis when requirements are pulled into a temp venv."""
+
+from fawltydeps.main import Analysis
+from fawltydeps.settings import Action, Settings
+
+
+def test_analysis_on_a_simple_project(write_tmp_files):
+    # create project
+    project_dir = write_tmp_files(
+        {
+            "requirements.txt": """\
+                pandas
+                click
+                """,
+            "python_file.py": "import pandas, django",
+        }
+    )
+    settings = Settings(
+        actions={Action.REPORT_UNDECLARED, Action.REPORT_UNUSED},
+        code=[project_dir],
+        deps=[project_dir],
+    )
+    analysis = Analysis.create(settings, pull_deps=True)
+
+    print("Verifying 'imports'...")
+    actual_imports = {i.name for i in analysis.imports}
+    expect_imports = set(["pandas", "django"])
+    assert actual_imports == expect_imports
+
+    print("Verifying 'dependencies'...")
+    actual_declared = {d.name for d in analysis.declared_deps}
+    expect_declared = set(["pandas", "click"])
+    assert actual_declared == expect_declared
+
+    print("Verifying 'undeclared_deps'...")
+    actual_undeclared = {u.name for u in analysis.undeclared_deps}
+    expect_undeclared = set(["django"])
+    assert actual_undeclared == expect_undeclared
+
+    print("Verifying 'unused_deps'...")
+    actual_unused = {u.name for u in analysis.unused_deps}
+    expect_unused = set(["click"])
+    assert actual_unused == expect_unused
