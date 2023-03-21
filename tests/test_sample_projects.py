e44af7bb4a4107db993fa3b34ec56ed2f31a33b8
Johan Herland <johan.herland@tweag.io>
2023-03-21 02:00:30 +0100

    test_sample_project: Refactor to use BaseProject/BaseExperiment
    This is a continuation of the previous commit: We now refactor
    test_sample_project to adopt the common base classes introduced in the
    previous commit. The new Experiment and SampleProject subclasses
    accurately portrays what makes sample_projects different from
    real_projects:
    
    - Experiment adds .code and .deps members to allow customization of the
      Settings object that is created for each experiment. Experiment also
      has a .settings() method to create this Settings object for use in an
      experiment.
    - SampleProject adds a .path member to point to the test/sample_projects
      subdir that contains this sample_project. It also implements the
      .collect() factory method to walk the subdirectories under
      test/sample_projects and create the corresponding objects.
    
    This leaves the actual test function to simply parametrize over the
    available projects/experiments, create an Analysis object with the
    appropriate settings, and verify the contents of the analysis.
    
    We add ample print() statements to help developers debug failing tests.
    
    The existing sample_projects TOML files are modified slightly to
    supported this refactoring: We rename the [analysis_result] section to
    [experiment.<something>], and add a description as well as a
    requirements list (empty in all current cases, as our existing
    sample_projects need only rely on the identity mapping).
    
    In sum, the objective of this refactoring is two-fold:
    - Increase sharing of common code between sample_projects and
      real_projects.
    - Teach sample_projects to be able to run multiple experiments per
      project, and have custom virtualenvs defined for each experiment.

--- tests/test_sample_projects.py
+++ tests/test_sample_projects.py
@@ -1,10 +1,9 @@
-"""
-Integration tests for the FawltyDeps project
+"""Verify behavior of FawltyDeps on sample projects.
 
-Check for given a simple project that following workflows work:
+Check for a given simple project that the following workflows work:
 - discover all imports and dependencies
   and correctly identify unused and missing dependencies
-- TODO given only part of the project to search in
+- given only part of the project to search in
   correctly identify unused and missing dependencies
 
 Sample projects are subdirectories of `tests/sample_projects`. These are
@@ -20,48 +19,95 @@ tests/sample_projects
     ├── expected.toml (mandatory)
     └── ... (regular Python project)
 """
-import sys
+from dataclasses import dataclass
 from pathlib import Path
+from typing import Iterator, List
 
 import pytest
 
 from fawltydeps.main import Analysis
 from fawltydeps.settings import Action, Settings
+from fawltydeps.types import TomlData
 
-from .project_helpers import AnalysisExpectations
-
-if sys.version_info >= (3, 11):
-    import tomllib  # pylint: disable=E1101
-else:
-    import tomli as tomllib
+from .project_helpers import BaseExperiment, BaseProject
 
 # These are (slow) integration tests that are disabled by default.
 pytestmark = pytest.mark.integration
 
 SAMPLE_PROJECTS_DIR = Path(__file__).with_name("sample_projects")
 
-sample_projects_params = [
-    pytest.param(sample_project, id=sample_project.name)
-    for sample_project in SAMPLE_PROJECTS_DIR.iterdir()
-    if sample_project.is_dir()
-]
+
+@dataclass
+class Experiment(BaseExperiment):
+    """A single experiment to run FawltyDeps on a sample project.
+
+    Input to the experiment consists of the following members:
+    - code: Settings.code paths relative to the sample project root
+    - deps: Settings.deps paths relative to the sample project root
+
+    See BaseExperiment for details on the inherited members.
+    """
+
+    code: List[str]
+    deps: List[str]
+
+    @classmethod
+    def parse_from_toml(cls, name: str, data: TomlData) -> "Experiment":
+        return cls(
+            code=data.get("code", [""]),
+            deps=data.get("deps", [""]),
+            **cls.init_args_from_toml(name, data),
+        )
+
+    def build_settings(self, project_path: Path, cache: pytest.Cache) -> Settings:
+        """Construct a Settings object appropriate for this experiment."""
+        return Settings(
+            actions={Action.REPORT_UNDECLARED, Action.REPORT_UNUSED},
+            code=[(project_path / path) for path in self.code],
+            deps=[(project_path / path) for path in self.deps],
+            pyenv=self.get_venv_dir(cache),
+        )
 
 
-@pytest.mark.parametrize("project_path", sample_projects_params)
-def test_integration_analysis_on_sample_projects__(project_path):
-    settings = Settings(
-        actions={Action.REPORT_UNDECLARED, Action.REPORT_UNUSED},
-        code=[project_path],
-        deps=[project_path],
-    )
+@dataclass
+class SampleProject(BaseProject):
+    """Encapsulate a sample project to be tested with FawltyDeps.
+
+    This represents a sample Python project living in a subdirectory under
+    tests/sample_projects/, and the things we expect FawltyDeps to find when
+    run on this project.
+
+    The actual data populating these objects is read from TOML files under
+    SAMPLE_PROJECTS_DIR.
+    """
+
+    path: Path  # Directory containing expected.toml, and rest of sample project
+
+    @classmethod
+    def collect(cls) -> Iterator["SampleProject"]:
+        for subdir in SAMPLE_PROJECTS_DIR.iterdir():
+            toml_path = subdir / "expected.toml"
+            if not toml_path.is_file():
+                continue
+            init_args, _ = cls.parse_toml(toml_path, Experiment)
+            yield cls(path=subdir, **init_args)
+
+
+@pytest.mark.parametrize(
+    "project, experiment",
+    [
+        pytest.param(project, experiment, id=experiment.name)
+        for project in SampleProject.collect()
+        for experiment in project.experiments
+    ],
+)
+def test_integration_analysis_on_sample_projects__(request, project, experiment):
+    print(f"Testing sample project: {project.name} under {project.path}")
+    print(f"Project description: {project.description}")
+    print()
+    print(f"Running sample project experiment: {experiment.name}")
+    print(f"Experiment description: {experiment.description}")
+    print()
+    settings = experiment.build_settings(project.path, request.config.cache)
     analysis = Analysis.create(settings)
-
-    with (project_path / "expected.toml").open("rb") as f:
-        experiment = tomllib.load(f)
-
-    print(experiment["project"].get("name"))
-    print(experiment["project"].get("description"))
-    expectation = AnalysisExpectations.parse_from_toml(
-        experiment.get("analysis_result", {})
-    )
-    expectation.verify_analysis(analysis)
+    experiment.expectations.verify_analysis(analysis)
