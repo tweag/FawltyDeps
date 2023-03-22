2fa4b97457400c09651a53729a7840da99e6553e
Maria Knorps <maria.knorps@tweag.io>
2023-03-22 15:54:54 +0100

    Add project with no issues to project samples

---
+++ tests/sample_projects/no_issues/python_file.py
@@ -0,0 +1,3 @@
+import pandas as pd
+import click
+import tensorflow
--- tests/test_cmdline_options.py
+++ tests/test_cmdline_options.py
@@ -3,29 +3,9 @@ import hypothesis.strategies as st
 import pytest
 from tests.test_cmdline import run_fawltydeps
 from itertools import product
+from .utils import SAMPLE_PROJECTS_DIR
 
-
-@pytest.fixture
-def project_without_errors(write_tmp_files):
-    return write_tmp_files(
-        {
-            "requirements.txt": """\
-                pandas
-                click
-                """,
-            "subdir/requirements.txt": """\
-                pandas
-                tensorflow>=2
-                """,
-            # This file should be ignored:
-            ".venv/requirements.txt": """\
-                foo_package
-                bar_package
-                """,
-            "python_file.py": "import pandas as pd \nimport click\nimport  tensorflow",
-        }
-    )
-
+project_with_no_issues = SAMPLE_PROJECTS_DIR / "no_issues"
 
 actions = [
     None,
@@ -36,20 +16,27 @@ actions = [
     "--list-deps",
 ]
 output_formats = [None, "--summary", "--detailed", "--json"]
+positional_arguments = [None, project_with_no_issues]
 
+actions_strategy = st.sampled_from(actions)
+output_formats_strategy = st.sampled_from(output_formats)
+positional_arguments_strategy = st.sampled_from(positional_arguments)
 
-@pytest.mark.parametrize("action,output_format", product(actions, output_formats))
+# @pytest.mark.parametrize("action,output_format", product(actions, output_formats))
+@given(
+    action=actions_strategy,
+    output_format=output_formats_strategy,
+    project_path=positional_arguments_strategy,
+)
 def test_options_interactions__correct_options__give_success_code(
-    action,
-    output_format,
-    project_without_errors,
+    action, output_format, project_path
 ):
     """Check if a combination of valid options
 
     makes a valid run of fawltydeps CLI tool.
     """
 
-    args = [f"{project_without_errors}"]
+    args = [project_path] if project_path is not None else []
     if action is not None:
         args.append(action)
     if output_format is not None:
