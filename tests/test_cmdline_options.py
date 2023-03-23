1eaa4aa33079912336cf45a8cc269d0fe5c8f0e4
Maria Knorps <maria.knorps@gmail.com>
2023-03-23 12:02:49 +0100

    Use all projects from sample projects to property based checks
    Use current project dir in a basepath instead of 'project_with_no_issues'
    
    Update no_issues project to the new format

--- tests/test_cmdline_options.py
+++ tests/test_cmdline_options.py
@@ -16,9 +16,11 @@ The strategy construction can be viewed as a reference
 to how the CLI options are expected to be used.
 """
 import io
+from pathlib import Path
 import string
 from functools import reduce
 from textwrap import dedent
+from typing import List
 
 import hypothesis.strategies as st
 from hypothesis import given
@@ -48,24 +50,7 @@ actions = [
     "--list-deps",
 ]
 output_formats = ["--summary", "--detailed", "--json"]
-# Options below are  adjusted to `project_with_no_issues`
-# They contain specific paths used in input and a chosen set of dependencies
-# TODO - create more general examples of happy path, including
-#        indicators of missing imports and dependencies
-available_python_input = [
-    str(f) for f in walk_dir(project_with_no_issues) if f.suffix == ".py"
-] + [CODE_STDIN_MARKER]
-# input files currently adjusted to `project_with_no_issues`
-available_deps = [
-    str(project_with_no_issues),
-    str(project_with_no_issues / "requirements.txt"),
-    str(project_with_no_issues / "subdir"),
-    str(project_with_no_issues / "subdir/requirements.txt"),
-]
-# currently adjusted to `project_with_no_issues` (only requirements.txt available)
-deps_parser_choice = [
-    "requirements.txt",
-]
+deps_parser_choice = ["requirements.txt", "setup.py", "setup.cfg", "pyproject.toml"]
 example_python_stdin = dedent(
     """\
     from pprint import pprint
@@ -78,36 +63,44 @@ example_python_stdin = dedent(
 venvs = [str(project_with_no_issues / ".venv")]
 
 
+# Options below contain specific paths used in input and a chosen set of dependencies
+def available_python_input(project_dir: Path) -> List[str]:
+    return [str(f) for f in walk_dir(project_dir) if f.suffix == ".py"] + [
+        CODE_STDIN_MARKER
+    ]
+
+
+def available_deps(project_dir: Path) -> List[str]:
+    return [str(f) for f in walk_dir(project_dir) if f.name != "expected.toml"]
+
+
 # ---------------------------------------------- #
 # Derive strategies from available options
 #
 # Each strategy is of type list[str]
 # ---------------------------------------------- #
-actions_strategy = st.one_of(st.sampled_from(actions), st.none()).map(
-    lambda x: [x] if x is not None else []
+actions_strategy = st.lists(st.sampled_from(actions), min_size=0, max_size=1)
+
+output_formats_strategy = st.lists(
+    st.sampled_from(output_formats), min_size=0, max_size=1
 )
 
-output_formats_strategy = st.one_of(st.sampled_from(output_formats), st.none()).map(
-    lambda x: [x] if x is not None else []
-)
 
-code_option_strategy = st.lists(
-    st.sampled_from(available_python_input),
-    min_size=0,
-    max_size=MAX_NUMER_OF_CODE_ARGS,
-).map(lambda xs: ["--code"] + xs if xs else [])
+def code_option_strategy(paths: List[str]):
+    return st.lists(
+        st.sampled_from(paths),
+        min_size=0,
+        max_size=MAX_NUMER_OF_CODE_ARGS,
+    ).map(lambda xs: ["--code"] + xs if xs else [])
 
-deps_option_strategy = (
-    st.lists(
-        st.sampled_from(available_deps),
+
+def deps_option_strategy(paths: List[str]):
+    return st.lists(
+        st.sampled_from(paths),
         min_size=0,
         max_size=MAX_NUMER_OF_DEPS_ARGS,
-        # TODO this is a temporary fix, to always result in return code = 0
-        # This test is supposed to give a "happy path"
-    )
-    .filter(lambda x: str(project_with_no_issues) in x or len(x) == 0)
-    .map(lambda xs: ["--deps"] + xs if xs else [])
-)
+    ).map(lambda xs: ["--deps"] + xs if xs else [])
+
 
 ignored_strategy = st.lists(safe_string, min_size=0, max_size=MAX_IGNORE_ARGS)
 ignored_undeclared_strategy = ignored_strategy.map(
@@ -139,28 +132,34 @@ venv_strategy = st.one_of(st.sampled_from(venvs), st.none()).map(
 @st.composite
 def cli_arguments_combinations(draw):
     to_stdin = None
+
+    project_dir = draw(
+        st.sampled_from([d for d in SAMPLE_PROJECTS_DIR.iterdir() if d.is_dir()])
+    )
+    code_paths = available_python_input(project_dir=project_dir)
+    dep_paths = available_deps(project_dir=project_dir)
     strategy = st.permutations(
         [
             draw(actions_strategy),
             draw(output_formats_strategy),
-            draw(deps_option_strategy),
+            draw(deps_option_strategy(dep_paths)),
             draw(ignored_undeclared_strategy),
             draw(ignored_unused_strategy),
             draw(deps_parser_choice_strategy),
             draw(verbosity_strategy),
-            draw(venv_strategy),
+            # draw(venv_strategy),
         ]
     )
     args = reduce(lambda x, y: x + y, draw(strategy))
 
     # only `code` option changes to_stdin
-    code = draw(code_option_strategy)
+    code = draw(code_option_strategy(code_paths))
     if code is not None:
         if "-" in code:
             to_stdin = example_python_stdin
         args += code
 
-    return (args, to_stdin)
+    return (project_dir, args, to_stdin)
 
 
 @given(cli_arguments=cli_arguments_combinations())
@@ -169,15 +168,13 @@ def test_options_interactions__correct_options__give_success_code(cli_arguments)
 
     makes a valid run of fawltydeps CLI tool.
     """
-    drawn_args, to_stdin = cli_arguments
+    project_dir, drawn_args, to_stdin = cli_arguments
     basepath = (
-        []
-        if {"--code", "--deps"}.issubset(set(drawn_args))
-        else [str(project_with_no_issues)]
+        [] if {"--code", "--deps"}.issubset(set(drawn_args)) else [str(project_dir)]
     )
     args = basepath + drawn_args
 
     with open("/dev/null", "w") as f_out:
         exit_code = main(cmdline_args=args, stdin=io.StringIO(to_stdin), stdout=f_out)
 
-    assert exit_code == 0
+    assert exit_code in {0, 3, 4}
