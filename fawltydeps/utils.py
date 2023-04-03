6ab412e379235aef35ccccffa1ab0975b9d1d908
Maria Knorps <maria.knorps@tweag.io>
2023-04-03 14:34:01 +0200

    Move 'calculated_once' to utils

--- fawltydeps/utils.py
+++ fawltydeps/utils.py
@@ -1,11 +1,15 @@
 """Common utilities"""
 
+from functools import wraps
 import os
 from dataclasses import is_dataclass
 from pathlib import Path
-from typing import Iterator, no_type_check
+from typing import Callable, Iterator, Optional, TypeVar, no_type_check
 
 import importlib_metadata
+
+Instance = TypeVar("Instance")
+T = TypeVar("T")
 
 
 @no_type_check
@@ -52,3 +56,31 @@ def hide_dataclass_fields(instance: object, *field_names: str) -> None:
         if name not in field_names
     }
     object.__setattr__(instance, "__dataclass_fields__", remaining_fields)
+
+
+def calculated_once(method: Callable[[Instance], T]) -> Callable[[Instance], T]:
+    """Emulate functools.cached_property for our simple use case.
+
+    functools.cached_property does not exist in Python v3.7, so we emulate the
+    simple things we need here:
+
+    Each method that uses this decorator will store its return value in an
+    instance attribute whose name is the method name prefixed with underscore.
+    The first time the property is referenced, the method will be called, its
+    return value stored in the corresponding instance attribute, and also
+    returned to the caller. All subsequent references (as long as the stored
+    value it not None) will return the instance attribute value directly,
+    without calling the method.
+    """
+
+    @wraps(method)
+    def wrapper(self: Instance) -> T:
+        cached_attr = f"_{method.__name__}"
+        cached_value: Optional[T] = getattr(self, cached_attr, None)
+        if cached_value is not None:
+            return cached_value
+        calculated: T = method(self)
+        setattr(self, cached_attr, calculated)
+        return calculated
+
+    return wrapper
