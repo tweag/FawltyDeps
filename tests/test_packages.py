7feb36316aadc361cfb99bc0d08b06450acaf7a5
Nour El Mawass <nour.elmawass@tweag.io>
2023-04-06 22:30:28 +0200

    Simplify parametrized tests
    Extract the different categories of dependencies in the parameters into
    their own lists/dicts:
    - non_locally_installed_deps
    - locally_installed_deps
    - user_defined_deps
    
    Define the toml-formatted user mapping from the user_defined_deps dict once
    
    Instead of manually definining the expected resolved dict of deps for
    each case, define a function that constructs this dict by taking advantage
    of the fact that the sets of deps extracted above are disjoint.

--- tests/test_packages.py
+++ tests/test_packages.py
@@ -1,7 +1,7 @@
 """Verify behavior of package lookup and mapping to import names."""
 
 import logging
-from textwrap import dedent
+from typing import Dict, List, Optional
 
 import pytest
 
@@ -220,71 +220,108 @@ def test_LocalPackageResolver_lookup_packages(dep_name, expect_import_names):
         assert actual[dep_name].import_names == expect_import_names
 
 
+# The deps in each category should be disjoint
+non_locally_installed_deps = ["pandas", "numpy", "other"]
+locally_installed_deps = {
+    "setuptools": [
+        "_distutils_hack",
+        "pkg_resources",
+        "setuptools",
+    ],
+    "pip": ["pip"],
+    "isort": ["isort"],
+}
+user_defined_deps = {"apache-airflow": ["airflow"]}
+
+user_defined_mapping = "\n".join(
+    [f'{dep} = ["{",".join(imports)}"]' for dep, imports in user_defined_deps.items()]
+)
+
+
+def generate_expected_resolved_deps(
+    locally_installed_deps: Optional[Dict[str, List[str]]] = None,
+    non_locally_installed_deps: Optional[List[str]] = None,
+    user_defined_deps: Optional[Dict[str, List[str]]] = None,
+):
+    ret = {}
+    if locally_installed_deps:
+        ret.update(
+            {
+                dep: Package(
+                    dep,
+                    {DependenciesMapping.LOCAL_ENV: set(imports)},
+                )
+                for dep, imports in locally_installed_deps.items()
+            }
+        )
+    if user_defined_deps:
+        ret.update(
+            {
+                dep: Package(
+                    dep,
+                    {DependenciesMapping.USER_DEFINED: set(imports)},
+                )
+                for dep, imports in user_defined_deps.items()
+            }
+        )
+    if non_locally_installed_deps:
+        ret.update(
+            {
+                dep: Package(dep, {DependenciesMapping.IDENTITY: {dep}})
+                for dep in non_locally_installed_deps
+            }
+        )
+    return ret
+
+
 @pytest.mark.parametrize(
     "dep_names,user_mapping,expected",
     [
         pytest.param([], None, {}, id="no_deps__empty_dict"),
         pytest.param(
-            ["pandas", "numpy", "other"],
+            non_locally_installed_deps,
             None,
-            {
-                "pandas": Package("pandas", {DependenciesMapping.IDENTITY: {"pandas"}}),
-                "numpy": Package("numpy", {DependenciesMapping.IDENTITY: {"numpy"}}),
-                "other": Package("other", {DependenciesMapping.IDENTITY: {"other"}}),
-            },
+            generate_expected_resolved_deps(
+                non_locally_installed_deps=non_locally_installed_deps
+            ),
             id="uninstalled_deps__use_identity_mapping",
         ),
         pytest.param(
-            ["setuptools", "pip", "isort"],
+            locally_installed_deps,
             None,
-            {
-                "setuptools": Package(
-                    "setuptools",
-                    {
-                        DependenciesMapping.LOCAL_ENV: {
-                            "_distutils_hack",
-                            "pkg_resources",
-                            "setuptools",
-                        }
-                    },
-                ),
-                "pip": Package("pip", {DependenciesMapping.LOCAL_ENV: {"pip"}}),
-                "isort": Package("isort", {DependenciesMapping.LOCAL_ENV: {"isort"}}),
-            },
+            generate_expected_resolved_deps(
+                locally_installed_deps=locally_installed_deps
+            ),
             id="installed_deps__use_local_env_mapping",
         ),
         pytest.param(
-            ["pandas", "pip"],
+            list(locally_installed_deps.keys()) + non_locally_installed_deps,
             None,
-            {
-                "pip": Package("pip", {DependenciesMapping.LOCAL_ENV: {"pip"}}),
-                "pandas": Package("pandas", {DependenciesMapping.IDENTITY: {"pandas"}}),
-            },
+            generate_expected_resolved_deps(
+                locally_installed_deps=locally_installed_deps,
+                non_locally_installed_deps=non_locally_installed_deps,
+            ),
             id="mixed_deps__uses_mixture_of_identity_and_local_env_mapping",
         ),
         pytest.param(
-            ["pandas", "pip", "apache_airflow"],
-            dedent(
-                """\
-                apache-airflow = ["airflow"]
-            """
+            non_locally_installed_deps
+            + list(locally_installed_deps.keys())
+            + list(user_defined_deps.keys()),
+            user_defined_mapping,
+            generate_expected_resolved_deps(
+                locally_installed_deps=locally_installed_deps,
+                non_locally_installed_deps=non_locally_installed_deps,
+                user_defined_deps=user_defined_deps,
             ),
-            {
-                "apache_airflow": Package(
-                    "apache-airflow", {DependenciesMapping.USER_DEFINED: {"airflow"}}
-                ),
-                "pip": Package("pip", {DependenciesMapping.LOCAL_ENV: {"pip"}}),
-                "pandas": Package("pandas", {DependenciesMapping.IDENTITY: {"pandas"}}),
-            },
             id="mixed_deps__uses_mixture_of_user_defined_identity_and_local_env_mapping",
         ),
         pytest.param(
-            ["pandas", "pip"],
-            dedent("""apache-airflow = ["airflow"]"""),
-            {
-                "pip": Package("pip", {DependenciesMapping.LOCAL_ENV: {"pip"}}),
-                "pandas": Package("pandas", {DependenciesMapping.IDENTITY: {"pandas"}}),
-            },
+            non_locally_installed_deps + list(locally_installed_deps.keys()),
+            user_defined_mapping,
+            generate_expected_resolved_deps(
+                locally_installed_deps=locally_installed_deps,
+                non_locally_installed_deps=non_locally_installed_deps,
+            ),
             id="mixed_deps__unaffected_by_nonmatching_user_defined_mapping",
         ),
     ],
@@ -298,7 +335,10 @@ def test_resolve_dependencies__focus_on_mappings(
         user_mapping_path = tmp_path / "mapping.toml"
 
     assert (
-        resolve_dependencies(dep_names, custom_mapping_path=user_mapping_path)
+        resolve_dependencies(
+            dep_names,
+            custom_mapping_path=user_mapping_path,
+        )
         == expected
     )
 
