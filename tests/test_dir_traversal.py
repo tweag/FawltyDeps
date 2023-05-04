c3fd4d6e5f6cad367cdc1fed18e46e8dc11c7b6f
Johan Herland <johan.herland@tweag.io>
2023-05-04 11:18:26 +0200

    utils: Add DirectoryTraversal, a more powerful alternative to walk_dir()
    walk_dir() is a simple wrapper around os.walk() that has served us well
    so far, however, our traverse_project module will soon have to deal with
    two new features that require something considerably more powerful than
    walk_dir()
    
     1. Supporting the auto-detection of Python environments: These are
        directories (where we have so far only searched for files), and we
        also need the detection of a Python environment directory to
        automatically _disable_ the traversal of the subdirectories beneath
        this directory, so that we _don't_ end up finding code and deps
        files inside a Python environment.
    
     2. Support ignoring files and directories in our traversal. These can
        be patterns coming from a file like e.g. .gitignore, or given
        directly on the command-line. More about this in a later PR.
    
    These two features (but especially the first) makes the interaction
    between traverse_project and the underlying directory traversal logic
    much more dynamic: We need to able to update details of the traversal
    (what we're looking for, or what we're ignoring) _while_ the traversal
    is ongoing.
    
    Additionally, both for correctness and performance, we want to ensure
    that we only traverse the project _once_, even when the given .code,
    .deps, or .pyenvs settings indicate a partially overlapping set of
    directories.
    
    The new DirectoryTraversal class tries to meet all these requirements
    while keeping the API as simple as possible:
    
    - Directories can be added to the traversal with .add()
    - Directories can be skipped/ignored with .ignore()
    - The traversal is performed by iterating over the result of .traverse()
      (similar to how os.walk() works).
    - While we expect most of the traversal to be set up with .add() and
      .ignore() _before_ .traverse() is called, we must still support calls
      to .add() and .ignore() _while_ we are iterating over the .traverse().
    - In .add(), we support _attaching_ metadata to the directories being
      traversed. When traversing, this metadata will be collected (for the
      current directory along with its parent directories), and passed to
      each traversal step.
    - Once a directory has been traversed, it will never be traversed again
      (by this DirectoryTraversal instance), specifically, calling .add() on
      a directory that we have already finished traversing will have no
      effect.
    - The traversal of directories is tracked using the device/inode numbers
      of the directories. Thus attempting to traverse a directory and a
      symlink to the same directory will result in the directory being
      traversed only _once_.
    - Tracking directories using device/inode numbers also makes us able to
      detect/prevent infinite traversal loops that would occur with mutually
      recursive symlinks, or symlinks pointing to parent directories.
    
    We include a comprehensive test suite that attempts to cover many corner
    cases of directory traversal as possible, and further document exactly
    how DirectoryTraversal works.

---
+++ tests/test_dir_traversal.py
@@ -0,0 +1,278 @@
+"""Test core functionality of DirectoryTraversal class."""
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Generic, List, Optional, Set, Tuple, TypeVar
+
+import pytest
+
+from fawltydeps.utils import DirectoryTraversal
+
+from .utils import assert_unordered_equivalence
+
+T = TypeVar("T")
+
+
+@dataclass
+class BaseEntry(ABC):
+    """Base class for a file system entry used in traversal tests."""
+
+    path: str
+
+    @abstractmethod
+    def __call__(self, tmp_path: Path):
+        raise NotImplementedError
+
+
+@dataclass
+class File(BaseEntry):
+    """A file with optional contents."""
+
+    contents: str = ""
+
+    def __call__(self, tmp_path: Path):
+        path = tmp_path / self.path
+        path.parent.mkdir(parents=True, exist_ok=True)
+        path.write_text(self.contents)
+
+
+@dataclass
+class Dir(BaseEntry):
+    """A directory."""
+
+    def __call__(self, tmp_path: Path):
+        path = tmp_path / self.path
+        path.mkdir(parents=True)
+
+
+@dataclass
+class RelativeSymlink(BaseEntry):
+    """A symlink to a relative path."""
+
+    target: str
+
+    def __call__(self, tmp_path: Path):
+        path = tmp_path / self.path
+        path.parent.mkdir(parents=True, exist_ok=True)
+        path.symlink_to(self.target)
+
+
+@dataclass
+class AbsoluteSymlink(BaseEntry):
+    """A symlink to an absolute path."""
+
+    target: str
+
+    def __call__(self, tmp_path: Path):
+        assert tmp_path.is_absolute()
+        path = tmp_path / self.path
+        path.parent.mkdir(parents=True, exist_ok=True)
+        path.symlink_to(tmp_path / self.target)
+
+
+@dataclass
+class ExpectedTraverseStep(Generic[T]):
+    """Expected data for one step of DirectoryTraversal."""
+
+    # All strings are relative to tmp_path
+    cur_dir: str
+    subdirs: List[str]
+    files: List[str]
+    attached: List[T]
+
+    def prepare(self, tmp_path: Path) -> Tuple[Path, Set[Path], Set[Path], List[T]]:
+        return (
+            tmp_path / self.cur_dir,
+            {tmp_path / self.cur_dir / d for d in self.subdirs},
+            {tmp_path / self.cur_dir / f for f in self.files},
+            self.attached,
+        )
+
+
+@dataclass
+class DirectoryTraversalVector(Generic[T]):
+    """Test vectors for DirectoryTraversal."""
+
+    id: str
+    given: List[BaseEntry]
+    add: List[Tuple[str, Optional[T]]] = field(default_factory=lambda: [(".", None)])
+    ignore: List[str] = field(default_factory=list)
+    expect: List[ExpectedTraverseStep] = field(default_factory=list)
+    expect_alternatives: Optional[List[List[ExpectedTraverseStep]]] = None
+
+
+directory_traversal_vectors: List[DirectoryTraversalVector] = [
+    DirectoryTraversalVector(
+        "empty_dir",
+        given=[],
+        expect=[ExpectedTraverseStep(".", [], [], [])],
+    ),
+    DirectoryTraversalVector(
+        "one_file__attach_data",
+        given=[File("foo")],
+        add=[(".", 123)],
+        expect=[ExpectedTraverseStep(".", [], ["foo"], [123])],
+    ),
+    DirectoryTraversalVector(
+        "one_subdir_plus__attach_data_on_both",
+        given=[Dir("sub")],
+        add=[(".", 123), ("sub", 456)],
+        expect=[
+            ExpectedTraverseStep(".", ["sub"], [], [123]),
+            ExpectedTraverseStep("sub", [], [], [123, 456]),
+        ],
+    ),
+    DirectoryTraversalVector(
+        "one_subdir__attach_data_twice_on_parent_dir",
+        given=[Dir("sub")],
+        add=[(".", 123), (".", 456)],
+        expect=[
+            ExpectedTraverseStep(".", ["sub"], [], [123, 456]),
+            ExpectedTraverseStep("sub", [], [], [123, 456]),
+        ],
+    ),
+    DirectoryTraversalVector(
+        "add_subdir__ignore_parent_with_data__traverse_only_subdir_with_no_data",
+        given=[Dir("sub")],
+        add=[(".", 123), ("sub", None)],
+        ignore=["."],
+        expect=[ExpectedTraverseStep("sub", [], [], [])],
+    ),
+    DirectoryTraversalVector(
+        "nested_subdir__attach_data_on_some_parents__gets_data_from_grandparents",
+        given=[Dir("a/b/c/d")],
+        add=[("a/b/c", 123), ("a", 456), (".", None)],
+        expect=[
+            ExpectedTraverseStep(".", ["a"], [], []),
+            ExpectedTraverseStep("a", ["b"], [], [456]),
+            ExpectedTraverseStep("a/b", ["c"], [], [456]),
+            ExpectedTraverseStep("a/b/c", ["d"], [], [456, 123]),
+            ExpectedTraverseStep("a/b/c/d", [], [], [456, 123]),
+        ],
+    ),
+    DirectoryTraversalVector(
+        "symlinks_to_self__are_not_traversed",
+        given=[RelativeSymlink("rel_self", "."), AbsoluteSymlink("abs_self", ".")],
+        expect=[ExpectedTraverseStep(".", ["rel_self", "abs_self"], [], [])],
+    ),
+    DirectoryTraversalVector(
+        "symlinks_to_parent__are_not_traversed",
+        given=[
+            RelativeSymlink("sub/rel_parent", ".."),
+            AbsoluteSymlink("sub/abs_parent", "."),
+        ],
+        expect=[
+            ExpectedTraverseStep(".", ["sub"], [], []),
+            ExpectedTraverseStep("sub", ["rel_parent", "abs_parent"], [], []),
+        ],
+    ),
+    DirectoryTraversalVector(
+        "mutual_symlinks__are_traversed_once",
+        given=[
+            RelativeSymlink("sub1/rel_link_sub2", "../sub2"),
+            AbsoluteSymlink("sub2/abs_link_sub1", "sub1"),
+        ],
+        expect_alternatives=[
+            [
+                ExpectedTraverseStep(".", ["sub1", "sub2"], [], []),
+                ExpectedTraverseStep("sub1", ["rel_link_sub2"], [], []),
+                ExpectedTraverseStep("sub2", ["abs_link_sub1"], [], []),
+            ],
+            [
+                ExpectedTraverseStep(".", ["sub1", "sub2"], [], []),
+                ExpectedTraverseStep("sub1", ["rel_link_sub2"], [], []),
+                ExpectedTraverseStep("sub1/rel_link_sub2", ["abs_link_sub1"], [], []),
+            ],
+            [
+                ExpectedTraverseStep(".", ["sub1", "sub2"], [], []),
+                ExpectedTraverseStep("sub2", ["abs_link_sub1"], [], []),
+                ExpectedTraverseStep("sub2/abs_link_sub1", ["rel_link_sub2"], [], []),
+            ],
+        ],
+    ),
+    DirectoryTraversalVector(
+        "relative_symlink_to_dir_elsewhere__is_traversed",
+        given=[
+            File("elsewhere/file"),
+            RelativeSymlink("here/symlink", "../elsewhere"),
+        ],
+        add=[("here", None)],
+        expect=[
+            ExpectedTraverseStep("here", ["symlink"], [], []),
+            ExpectedTraverseStep("here/symlink", [], ["file"], []),
+        ],
+    ),
+    DirectoryTraversalVector(
+        "absolute_symlink_to_dir_elsewhere__is_traversed",
+        given=[
+            File("elsewhere/file"),
+            AbsoluteSymlink("here/symlink", "elsewhere"),
+        ],
+        add=[("here", None)],
+        expect=[
+            ExpectedTraverseStep("here", ["symlink"], [], []),
+            ExpectedTraverseStep("here/symlink", [], ["file"], []),
+        ],
+    ),
+    DirectoryTraversalVector(
+        "dir_and_symlinks_to_dir__is_traversed_only_once",
+        given=[
+            File("dir/file"),
+            RelativeSymlink("rel_link", "./dir"),
+            AbsoluteSymlink("abs_link", "dir"),
+        ],
+        expect_alternatives=[
+            [
+                ExpectedTraverseStep(".", ["dir", "rel_link", "abs_link"], [], []),
+                ExpectedTraverseStep("dir", [], ["file"], []),
+            ],
+            [
+                ExpectedTraverseStep(".", ["dir", "rel_link", "abs_link"], [], []),
+                ExpectedTraverseStep("rel_link", [], ["file"], []),
+            ],
+            [
+                ExpectedTraverseStep(".", ["dir", "rel_link", "abs_link"], [], []),
+                ExpectedTraverseStep("abs_link", [], ["file"], []),
+            ],
+        ],
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "vector", [pytest.param(v, id=v.id) for v in directory_traversal_vectors]
+)
+def test_DirectoryTraversal(vector: DirectoryTraversalVector, tmp_path):
+    for entry in vector.given:
+        entry(tmp_path)
+
+    traversal: DirectoryTraversal = DirectoryTraversal()
+    for path, data in vector.add:
+        traversal.add(tmp_path / path, data)
+    for path in vector.ignore:
+        traversal.ignore(tmp_path / path)
+
+    actual = [
+        (cur_dir, set(subdirs), set(files), list(data))
+        for cur_dir, subdirs, files, data in traversal.traverse()
+    ]
+    if vector.expect_alternatives is None:  # vector.expect _must_ match
+        expect = [step.prepare(tmp_path) for step in vector.expect]
+        assert_unordered_equivalence(actual, expect)
+    else:  # one of the alternatives must match
+        for alternative in vector.expect_alternatives:
+            expect = [step.prepare(tmp_path) for step in alternative]
+            try:
+                assert_unordered_equivalence(actual, expect)
+            except AssertionError:  # this alternative failed
+                continue  # skip to next alternative
+            else:  # this alternative passed
+                break  # abort loop and skip below else clause
+        else:  # we exhausted all alternatives
+            assert False, f"None of the alternatives matched {actual}"
+
+
+def test_DirectoryTraversal__raises_error__when_adding_missing_dir(tmp_path):
+    traversal = DirectoryTraversal()
+    with pytest.raises(NotADirectoryError):
+        traversal.add(tmp_path / "MISSING")
