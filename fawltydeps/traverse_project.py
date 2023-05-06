ee6c5df0227e5fc7c1cfed60404ff6e421546206
Johan Herland <johan.herland@tweag.io>
2023-05-06 14:49:43 +0200

    traverse_project: Refactor find_sources() to DirectoryTraversal
    At this point, DirectoryTraversal is overkill for what find_sources() is
    doing, but we'll seen be integrating the auto-detection of Python
    environments into this code, and this preparation will make that
    addition much easier.

--- fawltydeps/traverse_project.py
+++ fawltydeps/traverse_project.py
@@ -1,18 +1,18 @@
 """Traverse a project to identify appropriate inputs to FawltyDeps."""
 import logging
 from pathlib import Path
-from typing import AbstractSet, Dict, Iterator, Optional, Set, Type
+from typing import AbstractSet, Iterator, Optional, Type, Union
 
 from fawltydeps.extract_declared_dependencies import validate_deps_source
 from fawltydeps.extract_imports import validate_code_source
 from fawltydeps.settings import Settings
 from fawltydeps.types import CodeSource, DepsSource, Source, UnparseablePathException
-from fawltydeps.utils import walk_dir
+from fawltydeps.utils import DirectoryTraversal
 
 logger = logging.getLogger(__name__)
 
 
-def find_sources(
+def find_sources(  # pylint: disable=too-many-branches
     settings: Settings,
     source_types: AbstractSet[Type[Source]] = frozenset([CodeSource, DepsSource]),
 ) -> Iterator[Source]:
@@ -22,18 +22,23 @@ def find_sources(
     and yield the corresponding *Source objects found.
     """
 
-    # Collect any directories we will need to traverse.
-    dirs_to_traverse: Dict[Path, Set[Type[Source]]] = {}
+    logger.debug("find_sources() Looking for sources under:")
+    logger.debug(f"    code:   {settings.code}")
+    logger.debug(f"    deps:   {settings.deps}")
+
+    traversal: DirectoryTraversal[Union[Type[Source], Path]] = DirectoryTraversal()
 
     for path_or_special in settings.code if CodeSource in source_types else []:
         # exceptions raised by validate_code_source() are propagated here
         validated: Optional[Source] = validate_code_source(path_or_special)
         if validated is not None:  # parse-able file given directly
+            logger.debug(f"find_sources() Found {validated}")
             yield validated
         else:  # must traverse directory
             # sanity check: convince mypy that SpecialPath is already handled
             assert isinstance(path_or_special, Path)
-            dirs_to_traverse.setdefault(path_or_special, set()).add(CodeSource)
+            traversal.add(path_or_special, CodeSource)
+            traversal.add(path_or_special, path_or_special)  # also record base dir
 
     for path in settings.deps if DepsSource in source_types else []:
         # exceptions raised by validate_deps_source() are propagated here
@@ -41,24 +46,34 @@ def find_sources(
             path, settings.deps_parser_choice, filter_by_parser=False
         )
         if validated is not None:  # parse-able file given directly
+            logger.debug(f"find_sources() Found {validated}")
             yield validated
         else:  # must traverse directory
-            dirs_to_traverse.setdefault(path, set()).add(DepsSource)
+            traversal.add(path, DepsSource)
 
-    for dir_path, types in dirs_to_traverse.items():
+    for _cur_dir, subdirs, files, extras in traversal.traverse():
+        for subdir in subdirs:  # don't recurse into dot dirs
+            if subdir.name.startswith("."):
+                traversal.ignore(subdir)
+
+        types = {t for t in extras if t in source_types}
         assert len(types) > 0
-        for file in walk_dir(dir_path):  # traverse directories only _once_
-            if CodeSource in types:
+        if CodeSource in types:
+            # Retrieve base_dir from closest ancestor, i.e. last Path in extras
+            base_dir = next((x for x in reversed(extras) if isinstance(x, Path)), None)
+            assert base_dir is not None  # sanity check: No CodeSource w/o base_dir
+            for path in files:
                 try:  # catch all exceptions while traversing dirs
-                    validated = validate_code_source(file, dir_path)
+                    validated = validate_code_source(path, base_dir)
                     assert validated is not None  # sanity check
                     yield validated
                 except UnparseablePathException:  # don't abort directory walk for this
                     pass
-            if DepsSource in types:
+        if DepsSource in types:
+            for path in files:
                 try:  # catch all exceptions while traversing dirs
                     validated = validate_deps_source(
-                        file, settings.deps_parser_choice, filter_by_parser=True
+                        path, settings.deps_parser_choice, filter_by_parser=True
                     )
                     assert validated is not None  # sanity check
                     yield validated
