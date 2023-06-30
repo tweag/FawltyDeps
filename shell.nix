c0ccedf62f1bbe668d2f0028d3c8376b76dfcecd
Johan Herland <johan.herland@tweag.io>
2023-06-30 15:06:48 +0200

    shell.nix: Update our development environment
    Before we can start thinking about supporting the upcoming Python v3.12
    release, we must upgrade our Nix-based dev shell to a version of Nixpkgs
    that supports/contains python312. However, there is no version of
    nixpkgs that supports _both_ Python v3.7 (which recently reached its
    end-of-life), and Python v3.12. This can be solved, however, by pulling
    in python37 from an older version of nixpkgs.
    
    This commit upgrades our shell.nix to a newer version of nixpkgs (while
    still pulling python37 from an older version). The new version of
    nixpkgs has also changed how it provides Poetry: Instead of pulling it
    as a Python package with _one_ of the Python versions we use, it is now
    consumed as a top-level package. As a side effect, we now start using a
    newer Poetry version:v1.4.2 [1]. The Python version that we use inside
    the default Poetry shell is then determined by which python executable
    we pass to the `poetry env use` command. For now, this remains the same
    Python v3.10 that we have been using for a while.
    
    [1]: The new Poetry version considerably reformats the poetry.lock file,
    but there should be no semantic changes to this file: the current
    version was generated by running `poetry lock --no-update` after
    entering the new nix-shell.

--- shell.nix
+++ shell.nix
@@ -1,25 +1,25 @@
 {
-  pkgs ? import (builtins.fetchTarball {
+  pkgsWith37 ? import (builtins.fetchTarball {
     # Branch: nixos-22.11
     url = "https://github.com/NixOS/nixpkgs/archive/96e18717904dfedcd884541e5a92bf9ff632cf39.tar.gz";
     sha256 = "0zw1851mia86xqxdf8jgy1c6fm5lqw4rncv7v2lwxar3vhpn6c78";
-  }) {}
+  }) {},
+  python37_overlay ? self: super: { python37 = pkgsWith37.python37; },
+  pkgs ? import (builtins.fetchTarball {
+    # Branch: nixos-23.05
+    url = "https://github.com/NixOS/nixpkgs/archive/9790f3242da2152d5aa1976e3e4b8b414f4dd206.tar.gz";
+    sha256 = "1y6zipys4803ckvnamfljb8raglgkbz1fz1fg03cxp4jqiiva5s1";
+  }) { overlays = [ python37_overlay ]; }
 }:
-let
-  python310WithPoetry = (
-    pkgs.python310.withPackages (pypkgs: with pypkgs; [
-      poetry
-    ])
-  );
-in
 pkgs.mkShell {
   name = "fawltydeps-env";
   buildInputs = with pkgs; [
     python37
     python38
     python39
-    python310WithPoetry
+    python310
     python311
+    poetry
   ];
   shellHook = ''
     # This is needed to keep python3.7 working while in the same nix-shell
@@ -28,7 +28,7 @@ pkgs.mkShell {
     unset _PYTHON_HOST_PLATFORM
     unset _PYTHON_SYSCONFIGDATA_NAME
 
-    poetry env use "${python310WithPoetry}/bin/python"
+    poetry env use "${pkgs.python310}/bin/python"
     poetry install --sync --with=dev
     source "$(poetry env info --path)/bin/activate"
   '';
