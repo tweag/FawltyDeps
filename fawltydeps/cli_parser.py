b6407f223badace92e6ae74a7d2ad746442215c5
Johan Herland <johan.herland@tweag.io>
2023-10-30 17:07:08 +0100

    Add --exclude-from option to CLI
    Include a few tests demonstrating the successful use of --exclude-from.
    Rely on unit tests in previous commits for verifying the underlying
    exclude-from functionality.

--- fawltydeps/cli_parser.py
+++ fawltydeps/cli_parser.py
@@ -192,6 +192,17 @@ def populate_parser_paths_options(parser: argparse._ActionsContainer) -> None:
         ),
     )
     parser.add_argument(
+        "--exclude-from",
+        nargs="+",
+        action="union",
+        type=Path,
+        metavar="PATH",
+        help=(
+            "Files containing exclude patterns to apply when looking for code"
+            " (imports), dependency declarations and/or Python environments."
+        ),
+    )
+    parser.add_argument(
         "--install-deps",
         dest="install_deps",
         action="store_true",
