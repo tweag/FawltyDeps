df73847e2fc559c0165399fe4ff38d5cd0d4617c
Maria Knorps <maria.knorps@tweag.io>
2024-01-23 12:22:06 +0100

    Collect all directories containing Python and notebook files

--- PyPI_analysis/detect_imports.py
+++ PyPI_analysis/detect_imports.py
@@ -281,9 +281,11 @@ def parse_code(
                 for alias in node.names:
                     name = alias.name.split(".", 1)[0]
                     if is_external_import(name):
-                        yield ParsedImport(
-                            name=name, source=source.supply(lineno=node.lineno)
-                        )
+                        yield {
+                            "Regular": ParsedImport(
+                                name=name, source=source.supply(lineno=node.lineno)
+                            )
+                        }
             elif isinstance(node, ast.ImportFrom):
                 logger.debug(ast.dump(node))
                 # Relative imports are always relative to the current package, and
@@ -292,9 +294,11 @@ def parse_code(
                 if node.level == 0 and node.module is not None:
                     name = node.module.split(".", 1)[0]
                     if is_external_import(name):
-                        yield ParsedImport(
-                            name=name, source=source.supply(lineno=node.lineno)
-                        )
+                        yield {
+                            "Regular": ParsedImport(
+                                name=name, source=source.supply(lineno=node.lineno)
+                            )
+                        }
 
     try:
         parsed_code = ast.parse(code, filename=str(source.path))
