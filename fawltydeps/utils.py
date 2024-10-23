8a63928da9d2fd4ea471dc459cd689dc5d682ac5
Johan Herland <johan.herland@tweag.io>
2024-10-23 02:13:42 +0200

    Remove obsolete @calculated_once decorator
    This decorator was a workaround for Python v3.7 missing
    functools.cached_property. Now that we no longer support v3.7, we can
    finally use the original from the standard library.

--- fawltydeps/utils.py
+++ fawltydeps/utils.py
@@ -3,9 +3,8 @@
 import logging
 import sys
 from dataclasses import is_dataclass
-from functools import wraps
 from pathlib import Path
-from typing import Callable, Iterator, Optional, TypeVar, no_type_check
+from typing import Iterator, TypeVar, no_type_check
 
 import importlib_metadata
 
@@ -45,34 +44,6 @@ def hide_dataclass_fields(instance: object, *field_names: str) -> None:
         if name not in field_names
     }
     object.__setattr__(instance, "__dataclass_fields__", remaining_fields)
-
-
-def calculated_once(method: Callable[[Instance], T]) -> Callable[[Instance], T]:
-    """Emulate functools.cached_property for our simple use case.
-
-    functools.cached_property does not exist in Python v3.7, so we emulate the
-    simple things we need here:
-
-    Each method that uses this decorator will store its return value in an
-    instance attribute whose name is the method name prefixed with underscore.
-    The first time the property is referenced, the method will be called, its
-    return value stored in the corresponding instance attribute, and also
-    returned to the caller. All subsequent references (as long as the stored
-    value it not None) will return the instance attribute value directly,
-    without calling the method.
-    """
-
-    @wraps(method)
-    def wrapper(self: Instance) -> T:
-        cached_attr = f"_{method.__name__}"
-        cached_value: Optional[T] = getattr(self, cached_attr, None)
-        if cached_value is not None:
-            return cached_value
-        calculated: T = method(self)
-        setattr(self, cached_attr, calculated)
-        return calculated
-
-    return wrapper
 
 
 def site_packages(venv_dir: Path = Path()) -> Path:
