9e4acc40ca1193ed467ccda80aea4107873f7e51
Johan Herland <johan.herland@tweag.io>
2025-02-25 19:32:15 +0100

    Update nix shell
    Now that we no longer support EOL Python versions, we can finally stop
    pulling Python version from multiple versions of nixpkgs. Instead, we
    choose a current version of nixos-unstable, and get all current Python
    versions from there. In addition this gives us a new version of Poetry,
    v2, which brings some associated changes.
    
    For one, Poetry since v2 no longer ships the `export` command, so its
    use in our noxfile.py necessitates adding the poetry-plugin-export
    dependency whenever nox is installed.

--- shell.nix
+++ shell.nix
@@ -1,22 +1,13 @@
 {
-  pkgsWithOldPythons ? import (builtins.fetchTarball {
-    # Branch: nixos-22.11
-    url = "https://github.com/NixOS/nixpkgs/archive/96e18717904dfedcd884541e5a92bf9ff632cf39.tar.gz";
-    sha256 = "0zw1851mia86xqxdf8jgy1c6fm5lqw4rncv7v2lwxar3vhpn6c78";
-  }) {},
-  old_pythons_overlay ? self: super: {
-    python38 = pkgsWithOldPythons.python38;
-  },
   pkgs ? import (builtins.fetchTarball {
     # Branch: nixos-unstable
-    url = "https://github.com/NixOS/nixpkgs/archive/62939616bcc4da119f15eed184b124a9383fcf56.tar.gz";
-    sha256 = "1nl40n5bbnzwyx1074g38py638s55d2bsn04ynlz9ix1r5f0nv6x";
-  }) { overlays = [ old_pythons_overlay ]; }
+    url = "https://github.com/NixOS/nixpkgs/archive/0196c0175e9191c474c26ab5548db27ef5d34b05.tar.gz";
+    sha256 = "sha256-WGaHVAjcrv+Cun7zPlI41SerRtfknGQap281+AakSAw=";
+  }) {}
 }:
 pkgs.mkShell {
   name = "fawltydeps-env";
   buildInputs = with pkgs; [
-    python38
     python39
     python310
     python311
@@ -30,8 +21,8 @@ pkgs.mkShell {
     pythonManylinuxPackages.manylinux2014
   ];
   shellHook = ''
-    poetry env use "${pkgs.python312}/bin/python"
-    poetry install --sync --with=dev
+    poetry env use "${pkgs.python313}/bin/python"
+    poetry sync --with=dev
     # Patch binaries in the Poetry virtualenv to link against Nix deps
     autoPatchelf "$(poetry env info --path)"
     source "$(poetry env info --path)/bin/activate"
